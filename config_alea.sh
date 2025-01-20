6dc62200df3c99d4a0fbeed7b0998138543f6f7d
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2025-01-20 10:32:01 -0500

    Update config_alea.sh

--- config_alea.sh
+++ config_alea.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #> Critical Folder Locations
- export CMAQ_HOME=/home/ubuntu/CMAQ
- export CMAQ_REPO=/home/ubuntu/CMAQ
+ export CMAQ_HOME=/home/ubuntu/ALEA
+ export CMAQ_REPO=/home/ubuntu/ALEA
  export CMAQ_DATA=$CMAQ_HOME/data
  [ ! -d $CMAQ_DATA ] && mkdir -p $CMAQ_DATA
 
