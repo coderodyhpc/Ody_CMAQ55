f11e54e464f7532323eeffa0f14cde2cc6d82dc2
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2025-02-19 10:11:57 -0500

    Update bldit_cctm.sh

--- CCTM/scripts/bldit_cctm.sh
+++ CCTM/scripts/bldit_cctm.sh
@@ -65,7 +65,7 @@ ParOpt=true             #> uncomment to build a multiple processor (MPI) executa
 #build_twoway=True                     #> uncomment to build WRF-CMAQ twoway;
                                        #>   comment out for off-line chemistry
 #> Two-way MPAS-CMAQ
-Build_mpas_cmaq=True
+build_mpas_cmaq=True
 #> Potential vorticity free-troposphere O3 scaling
 #potvortO3=True
 
@@ -184,9 +184,9 @@ Build_mpas_cmaq=True
  echo "End COMPUTING SYSTEM CONF"
  echo " "
 
-#============================================================================================
+#==================================================================================
 #> Implement User Input
-#============================================================================================
+#==================================================================================
 
 #> Check for CMAQ_REPO and CMAQ_LIB settings:
  if [ ! -e $CMAQ_REPO ] && [ ! -e $CMAQ_LIB ]
